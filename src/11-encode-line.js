9e33ae838b84b777426d8a7232f6d1082eb3d37f
mari-shakhanava <maria.shakhanava@gmail.com>
2021-04-11 15:00:06 +0300

    feat: add 11-encode-line solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,32 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  let counter = 1;
+  if (!str) return str;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === str.charAt(i + 1)) {
+      counter++;
+    } else {
+      result += `${counter}${str[i]}`;
+      counter = 1;
+    }
+  }
+  return result.split('').filter((x) => +x !== 1).join('');
 }
 
 module.exports = encodeLine;
+
+// function encodeLine(str) {
+//   const newObj = str.split('').reduce((acc, curr) => {
+//     if (typeof acc[curr] === 'undefined') {
+//       acc[curr] = 1;
+//     } else {
+//       acc[curr] += 1;
+//     }
+//     return acc;
+//   }, {});
+//   return Object.entries(newObj).map((x) =>
+//   x.reverse().filter((el) => el !== 1).join('')).join('');
+// }
