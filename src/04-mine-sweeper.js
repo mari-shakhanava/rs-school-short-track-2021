2a72b76df0bb13824b093b0a722ad43beba4b6a9
mari-shakhanava <maria.shakhanava@gmail.com>
2021-04-12 02:10:11 +0300

    feat: add 04-mine-sweeper solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,29 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const field = matrix.slice();
+  const yLength = matrix.length;
+  const xLength = matrix[0].length;
+  const saperOutput = [];
+  for (let i = 0; i < yLength; i++) {
+    const saperOutputRow = [];
+    for (let j = 0; j < xLength; j++) {
+      let counter = 0;
+      if (j !== 0 && field[i][j - 1] === true) counter++;
+      if (j !== 0 && i !== 0 && field[i - 1][j - 1] === true) counter++;
+      if (i !== 0 && field[i - 1][j] === true) counter++;//
+      if (field[j + 1] !== undefined && i !== 0 && field[i - 1][j + 1] === true) counter++;
+      if (field[i][j + 1] !== undefined && field[i][j + 1] === true) counter++;
+      if (field[i + 1] !== undefined
+        && field[i + 1][j + 1] !== undefined && field[i + 1][j + 1] === true) counter++;
+      if (field[i + 1] !== undefined && field[i + 1][j] === true) counter++;
+      if (field[i + 1] !== undefined && j !== 0 && field[i + 1][j - 1] === true) counter++;
+      saperOutputRow.push(counter);
+    }
+    saperOutput.push(saperOutputRow);
+  }
+  return saperOutput;
 }
 
 module.exports = minesweeper;
